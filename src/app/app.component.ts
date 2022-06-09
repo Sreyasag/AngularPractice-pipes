80d744a6b312b57b656aacaae1698e09ac984be5
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-06-09 09:47:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '4.PipesPractice';
+  name = "";
+  onChangeName(event:string){
+    this.name= event;
+  }
 }
